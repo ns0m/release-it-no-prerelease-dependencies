1d5d258772f994aa454cb02391dcc785fd23a4ed
Alexis THOMAS <fr.alexisthomas@gmail.com>
2021-11-24 00:25:25 +0100

    do check on prerelease dependencies: implement, test and coverage

--- index.js
+++ index.js
@@ -1,5 +1,62 @@
+const { EOL } = require('os');
+const path = require('path');
 const { Plugin } = require('release-it');
+const { hasAccess } = require('release-it/lib/util');
+const semver = require('semver');
 
-class NoPreReleaseDependenciesPlugin extends Plugin {}
+const MANIFEST_PATH = './package.json';
+const MODULES_PATH = './node_modules';
+
+class NoPreReleaseDependenciesPlugin extends Plugin {
+  static isEnabled(options) {
+    return hasAccess(MANIFEST_PATH) && hasAccess(MODULES_PATH) && options !== false;
+  }
+
+  init() {
+    if (this.hasPreReleaseDependencies()) {
+      const { preReleaseDependencies } = this.getContext();
+      throw new Error(
+        `Dependencies must not have pre-release version:${EOL}${JSON.stringify(preReleaseDependencies, null, 2)}`
+      );
+    }
+  }
+
+  isPreRelease(version) {
+    return Boolean(semver.prerelease(version));
+  }
+
+  hasPreReleaseDependencies() {
+    const { dependencies = {}, devDependencies = {} } = require(path.resolve(MANIFEST_PATH));
+    const preReleaseDepsInfo = this.getPreReleaseDependenciesInfo(dependencies);
+    const preReleaseDevDepsInfo = this.getPreReleaseDependenciesInfo(devDependencies);
+    this.setContext({
+      preReleaseDependencies: {
+        dependencies: preReleaseDepsInfo,
+        devDependencies: preReleaseDevDepsInfo
+      }
+    });
+    return Object.keys(preReleaseDepsInfo).length > 0 || Object.keys(preReleaseDevDepsInfo).length > 0;
+  }
+
+  getPreReleaseDependenciesInfo(dependencies) {
+    return Object.fromEntries(
+      Object.entries(dependencies)
+        .map(this.getDependencyEntryVersions, this)
+        .filter(([, { installed }]) => {
+          return Boolean(installed) && this.isPreRelease(installed);
+        })
+    );
+  }
+
+  getDependencyEntryVersions([packageName, versionRange]) {
+    let installedVersion;
+    try {
+      installedVersion = require(path.resolve(MODULES_PATH, packageName, MANIFEST_PATH)).version;
+    } catch (err) {
+      this.log.warn(`Failed to get installed version for dependency ${packageName}`);
+    }
+    return [packageName, { range: versionRange, installed: installedVersion }];
+  }
+}
 
 module.exports = NoPreReleaseDependenciesPlugin;
--- test.js
+++ test.js
@@ -1,9 +1,193 @@
+const os = require('os');
+const fs = require('fs');
+const path = require('path');
 const test = require('bron');
-const assert = require('assert');
+const assert = require('assert').strict;
+const sinon = require('sinon');
+const { vol } = require('memfs');
+const { patchFs, patchRequire } = require('fs-monkey');
 const { factory, runTasks } = require('release-it/test/util');
 const Plugin = require('.');
 
-test('should not throw', async t => {
+const workingDir = process.cwd();
+const testDir = fs.mkdtempSync(path.join(os.tmpdir(), 'rit-plugin-test-'));
+process.chdir(testDir);
+process.addListener('exit', () => {
+  process.chdir(workingDir);
+  fs.rmSync(testDir, { recursive: true, force: true });
+});
+patchFs(vol);
+patchRequire(vol, true);
+
+const initVolume = (volumeJson) => {
+  vol.reset();
+  Object.keys(require.cache)
+    .filter((key) => key.includes(path.basename(testDir)))
+    .forEach((key) => {
+      delete require.cache[key];
+    });
+  vol.fromJSON(volumeJson);
+};
+
+test('isEnabled true with manifest and modules', (t) => {
+  initVolume({
+    './package.json': JSON.stringify({ dependencies: {}, devDependencies: {} }),
+    './node_modules': null
+  });
+  assert.equal(Plugin.isEnabled(), true);
+});
+
+test('isEnabled false without manifest', (t) => {
+  initVolume({
+    './node_modules': null
+  });
+  assert.equal(Plugin.isEnabled(), false);
+});
+
+test('isEnabled false without modules', (t) => {
+  initVolume({
+    './package.json': JSON.stringify({ dependencies: {}, devDependencies: {} })
+  });
+  assert.equal(Plugin.isEnabled(), false);
+});
+
+test('isEnabled false if options=false', (t) => {
+  initVolume({
+    './package.json': JSON.stringify({ dependencies: {}, devDependencies: {} }),
+    './node_modules': null
+  });
+  assert.equal(Plugin.isEnabled(false), false);
+});
+
+test('should not throw if no preRelease dependencies', async (t) => {
   const plugin = factory(Plugin);
+  sinon.stub(plugin, 'hasPreReleaseDependencies').returns(false);
   await assert.doesNotReject(runTasks(plugin));
+});
+
+test('should throw if preRelease dependencies', async (t) => {
+  const plugin = factory(Plugin);
+  sinon.stub(plugin, 'hasPreReleaseDependencies').returns(true);
+  sinon.stub(plugin, 'getContext').returns({
+    preReleaseDependencies: {
+      dependencies: {},
+      devDependencies: {
+        [DEP_INSTALLED_PRE.name]: { range: DEP_INSTALLED_PRE.range, installed: DEP_INSTALLED_PRE.installed }
+      }
+    }
+  });
+  await assert.rejects(runTasks(plugin));
+});
+
+test('isPreRelease', (t) => {
+  const plugin = factory(Plugin);
+  assert.equal(plugin.isPreRelease('1.0.0-beta.0'), true);
+  assert.equal(plugin.isPreRelease('1.0.0'), false);
+});
+
+const DEP_INSTALLED = { name: 'dep-installed', range: '^1.0.0', installed: '1.1.1' };
+const DEP_INSTALLED_PRE = { name: 'dep-installed-pre', range: '^3.0.0-pre', installed: '3.0.0-beta.3' };
+const DEP_NOT_INSTALLED = { name: 'dep-not-installed', range: '*', installed: undefined };
+
+test('hasPreReleaseDependencies false', (t) => {
+  initVolume({
+    './package.json': JSON.stringify({ dependencies: { [DEP_INSTALLED.name]: DEP_INSTALLED.range } })
+  });
+  const plugin = factory(Plugin);
+  sinon
+    .stub(plugin, 'getPreReleaseDependenciesInfo')
+    .withArgs({ [DEP_INSTALLED.name]: DEP_INSTALLED.range })
+    .returns({})
+    .withArgs({})
+    .returns({});
+  const spySetContext = sinon.spy(plugin, 'setContext');
+  const hasPreReleaseDeps = plugin.hasPreReleaseDependencies();
+  assert.equal(hasPreReleaseDeps, false);
+  assert.equal(spySetContext.callCount, 1);
+  assert.deepEqual(spySetContext.getCall(0).args, [
+    {
+      preReleaseDependencies: {
+        dependencies: {},
+        devDependencies: {}
+      }
+    }
+  ]);
+});
+
+test('hasPreReleaseDependencies true', (t) => {
+  initVolume({
+    './package.json': JSON.stringify({ devDependencies: { [DEP_INSTALLED_PRE.name]: DEP_INSTALLED_PRE.range } })
+  });
+  const plugin = factory(Plugin);
+  sinon
+    .stub(plugin, 'getPreReleaseDependenciesInfo')
+    .withArgs({})
+    .returns({})
+    .withArgs({ [DEP_INSTALLED_PRE.name]: DEP_INSTALLED_PRE.range })
+    .returns({ [DEP_INSTALLED_PRE.name]: { range: DEP_INSTALLED_PRE.range, installed: DEP_INSTALLED_PRE.installed } });
+  const spySetContext = sinon.spy(plugin, 'setContext');
+  const hasPreReleaseDeps = plugin.hasPreReleaseDependencies();
+  assert.equal(hasPreReleaseDeps, true);
+  assert.equal(spySetContext.callCount, 1);
+  assert.deepEqual(spySetContext.getCall(0).args, [
+    {
+      preReleaseDependencies: {
+        dependencies: {},
+        devDependencies: {
+          [DEP_INSTALLED_PRE.name]: { range: DEP_INSTALLED_PRE.range, installed: DEP_INSTALLED_PRE.installed }
+        }
+      }
+    }
+  ]);
+});
+
+test('getPreReleaseDependenciesInfo', (t) => {
+  const plugin = factory(Plugin);
+  sinon
+    .stub(plugin, 'getDependencyEntryVersions')
+    .withArgs([DEP_INSTALLED.name, DEP_INSTALLED.range])
+    .returns([DEP_INSTALLED.name, { range: DEP_INSTALLED.range, installed: DEP_INSTALLED.installed }])
+    .withArgs([DEP_INSTALLED_PRE.name, DEP_INSTALLED_PRE.range])
+    .returns([DEP_INSTALLED_PRE.name, { range: DEP_INSTALLED_PRE.range, installed: DEP_INSTALLED_PRE.installed }])
+    .withArgs([DEP_NOT_INSTALLED.name, DEP_NOT_INSTALLED.range])
+    .returns([DEP_NOT_INSTALLED.name, { range: DEP_NOT_INSTALLED.range, installed: DEP_NOT_INSTALLED.installed }]);
+  sinon
+    .stub(plugin, 'isPreRelease')
+    .withArgs(DEP_INSTALLED.installed)
+    .returns(false)
+    .withArgs(DEP_INSTALLED_PRE.installed)
+    .returns(true);
+  const preReleaseInfo = plugin.getPreReleaseDependenciesInfo({
+    [DEP_INSTALLED.name]: DEP_INSTALLED.range,
+    [DEP_INSTALLED_PRE.name]: DEP_INSTALLED_PRE.range,
+    [DEP_NOT_INSTALLED.name]: DEP_NOT_INSTALLED.range
+  });
+  assert.deepEqual(preReleaseInfo, {
+    [DEP_INSTALLED_PRE.name]: { range: DEP_INSTALLED_PRE.range, installed: DEP_INSTALLED_PRE.installed }
+  });
+});
+
+test('getDependencyEntryVersions', (t) => {
+  initVolume({
+    './package.json': JSON.stringify({
+      dependencies: { [DEP_INSTALLED.name]: DEP_INSTALLED.range },
+      devDependencies: { [DEP_NOT_INSTALLED.name]: DEP_NOT_INSTALLED.range }
+    }),
+    [`./node_modules/${DEP_INSTALLED.name}/package.json`]: JSON.stringify({
+      name: DEP_INSTALLED.name,
+      version: DEP_INSTALLED.installed
+    })
+    // no DEP_NOT_INSTALLED on purpose
+  });
+  const plugin = factory(Plugin);
+  const depInstalledVersions = plugin.getDependencyEntryVersions([DEP_INSTALLED.name, DEP_INSTALLED.range]);
+  const depNotInstalledVersions = plugin.getDependencyEntryVersions([DEP_NOT_INSTALLED.name, DEP_NOT_INSTALLED.range]);
+  assert.deepEqual(depInstalledVersions, [
+    DEP_INSTALLED.name,
+    { range: DEP_INSTALLED.range, installed: DEP_INSTALLED.installed }
+  ]);
+  assert.deepEqual(depNotInstalledVersions, [
+    DEP_NOT_INSTALLED.name,
+    { range: DEP_NOT_INSTALLED.range, installed: DEP_NOT_INSTALLED.installed }
+  ]);
 });
